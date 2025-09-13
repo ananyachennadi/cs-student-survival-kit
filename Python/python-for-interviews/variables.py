7147dc504b8d11fc2df70b9f2e948dfa31d20353
Ananya Chennadi <ananyachennadi2@gmail.com>
2025-09-13 13:57:45 +0100

    feat: added code samples for variables (python)

---
+++ Python/python-for-interviews/variables.py
@@ -0,0 +1,17 @@
+# assigning multiple variables
+x, z = 2, 4
+print(x, z)
+
+# shorthand way of incrementing and decrementing
+x += 2
+print(x)
+x -= 2
+print(x)
+
+# x = x * 5
+x *= 5
+print(x)
+
+# None = null 
+x = None
+print(x)
